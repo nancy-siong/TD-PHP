3d2af81645ae5ecb6c12cca270e8d5b80a9bd1b1
Nancy Siong <nancy.siong@etu.umontpellier.fr>
2021-09-17 12:15:27 +0200

    fin exo 4

--- TD2/Model.php
+++ TD2/Model.php
@@ -14,7 +14,20 @@ class Model {
 		$password = Conf::getPassword();
 
 
-		self::$pdo = new PDO("mysql:host=$hostname; dbname=$database_name", $login, $password);
+		try{
+			/*
+			self::$pdo = new PDO("mysql:host=$hostname; dbname=$database_name", $login, $password);*/
+			self::$pdo = new PDO("mysql:host=$hostname;dbname=$database_name", $login, $password,
+    		array(PDO::MYSQL_ATTR_INIT_COMMAND => "SET NAMES utf8"));
+    		self::$pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+
+		} 
+
+		catch(PDOException $e) {
+		  echo $e->getMessage(); // affiche un message d'erreur
+		  die();
+		}
+
 
 	}
 
@@ -26,5 +39,6 @@ class Model {
 		return self::$pdo;
 	}
 }
+
 ?>
    
