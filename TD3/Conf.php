81d1927fd0dcb1a47e2aa3bab00467cd640153fa
Nancy Siong <nancy.siong@etu.umontpellier.fr>
2021-09-24 11:42:39 +0200

    fin exo 2

---
+++ TD3/Conf.php
@@ -0,0 +1,39 @@
+<?php
+class Conf {
+   
+  static private $databases = array(
+    // Le nom d'hote est webinfo a l'IUT
+    // ou localhost sur votre machine
+    'hostname' => 'webinfo',
+    // A l'IUT, vous avez une BDD nommee comme votre login
+    // Sur votre machine, vous devrez creer une BDD
+    'database' => 'siongn',
+    // A l'IUT, c'est votre login
+    // Sur votre machine, vous avez surement un compte 'root'
+    'login' => 'siongn',
+    // A l'IUT, c'est votre mdp (INE par defaut)
+    // Sur votre machine personelle, vous avez creez ce mdp a l'installation
+    'password' => 'phpmysql'
+  );
+   
+  static public function getLogin() {
+    return self::$databases['login'];
+  }
+
+  static public function getHostname() {
+    return self::$databases['hostname'];
+  }
+
+  static public function getDatabase() {
+    return self::$databases['database'];
+  }
+
+  static public function getPassword() {
+    return self::$databases['password'];
+  }
+
+
+   
+}
+?>
+
---
+++ TD3/lireVoiture.php
@@ -0,0 +1,50 @@
+<?php
+
+require_once 'Model.php';
+require_once 'Voiture.php';
+
+/* EXO 6
+$pdo = Model::getPDO();
+
+$rep = $pdo->query("SELECT * FROM Voiture;");
+
+$tab_obj = $rep->fetchAll(PDO::FETCH_OBJ)
+
+foreach ($tab_obj as $value) {
+    echo $value->marque . "<br>";
+}
+
+*/
+
+
+/* EXO 7
+$pdo = Model::getPDO();
+
+$rep->setFetchMode(PDO::FETCH_CLASS, 'Voiture');
+
+$tab_voit = $rep->fetchAll();
+
+foreach ($tab_voit as $value) {
+    echo $value->afficher();
+}
+
+
+*/
+
+//EXO 8
+$tab_voit = Voiture::getAllVoitures();
+
+foreach ($tab_voit as $value) {
+    $value->afficher();
+    echo "<br>";
+}
+
+
+echo Voiture::getVoitureByImmat('1111AA11')->afficher();
+
+
+$voiture = new Voiture('Ford', 'Blanc', '3333AA11' );
+$voiture->save();
+
+
+?>
