9b321b692a99c728133bbb02c211f9e0fbf1bc03
Nancy Siong <nancy.siong@etu.umontpellier.fr>
2021-09-24 12:02:39 +0200

    fin ex3

--- TD3/creerVoiture.php
+++ TD3/creerVoiture.php
@@ -1,9 +1,17 @@
 <?php
 
-    if (!empty($_POST)) {
-        echo $_POST['immatriculation']. "<br>";
-        echo $_POST['marque'] . "<br>";
-        echo $_POST['couleur'];
-    }
+require_once 'Voiture.php';
+
+if (!empty($_POST)) {
+    echo $_POST['immatriculation']. "<br>";
+    echo $_POST['marque'] . "<br>";
+    echo $_POST['couleur'];
+}
+
+$voiture = new Voiture($_POST['marque'], $_POST['couleur'], $_POST['immatriculation']);
+
+$voiture->save();
+
+
 
 ?>
