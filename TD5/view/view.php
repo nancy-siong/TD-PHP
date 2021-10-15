091942fbe599cd77cdbf2a46ba71b8ff650c748a
Nancy Siong <nancy.siong@etu.umontpellier.fr>
2021-10-15 11:22:06 +0200

    fin exo 8

--- TD5/view/view.php
+++ TD5/view/view.php
@@ -4,13 +4,25 @@
         <meta charset="UTF-8">
         <title><?php echo $pagetitle; ?></title>
     </head>
+
     <body>
-<?php
-// Si $controleur='voiture' et $view='list',
-// alors $filepath="/chemin_du_site/view/voiture/list.php"
-$filepath = File::build_path(array("view", $controller, "$view.php"));
-require $filepath;
-?>
+        <nav>
+            <ul>
+                <li><a href=" index.php?action=readAll&controller=utilisateur"> Accueil </a></li>
+                <li><a href="index.php?action=readAll"> Liste des voitures </a></li>
+                <li><a href="index.php?action=readAll&controller=trajet"> Liste des trajets </a></li>
+            </ul>
+        </nav>
+        <?php
+            $filepath = File::build_path(array("view", $controller, "$view.php"));
+            require $filepath;
+        ?>
+
+        <p style="border: 1px solid black;text-align:right;padding-right:1em;">
+        Site de covoiturage de ...
+        </p>
+
+
     </body>
 </html>
 
